e7b286d3933a46ebec8f6987eb2b1aea5ae48e18
Lars Angel Høydahl Ohme <larsah@uia.no>
2025-09-03 20:10:02 +0200

    Commit for creating second Assignment

---
+++ assignment_2/main.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+from nltk.sem.chat80 import borders
+
+
+def padding(image, border_width):
+
+
+def main():
+    image = cv2.imread("lena-2.png")
+    border_width = 100
+    padding(image, border_width)
+
+
+if __name__ == '__main__':
+    main()
