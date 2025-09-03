a41a88decffcda1eb4f5d77b656a1f7034c41770
Lars Angel Høydahl Ohme <larsah@uia.no>
2025-09-03 20:36:04 +0200

    Appended padding function

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -1,15 +1,22 @@
 import cv2
 import numpy as np
-from nltk.sem.chat80 import borders
-
+import matplotlib.pyplot as plt
 
 def padding(image, border_width):
-
+    reflect = cv2.copyMakeBorder(image,border_width,border_width,border_width,border_width,cv2.BORDER_REFLECT)
+    print(image.shape)
+    print(reflect.shape)
+    print(cv2.imshow('reflect', reflect))
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 def main():
     image = cv2.imread("lena-2.png")
     border_width = 100
-    padding(image, border_width)
+    padding(image, border_width) # Task 1
+
+
+
 
 
 if __name__ == '__main__':
